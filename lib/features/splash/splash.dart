90b7ff0a42b3ef5050d1e694c4aaae1341744583
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2023-12-10 00:03:42 +0100

    Remove unused imports in splash.dart

--- lib/features/splash/splash.dart
+++ lib/features/splash/splash.dart
@@ -2,9 +2,7 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:spa_app/routes.dart';
-import 'package:spa_app/utils/app_colors.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
