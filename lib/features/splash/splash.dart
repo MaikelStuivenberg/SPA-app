c1d56a9663d137fa99ddd5fff514247d79b14429
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2023-12-09 20:29:02 +0100

    Update dependencies and fix splash screen background

--- lib/features/splash/splash.dart
+++ lib/features/splash/splash.dart
@@ -13,46 +13,62 @@ class SplashPage extends StatefulWidget {
 }
 
 class SplashPageState extends State<SplashPage> {
+  bool _visible = true;
+
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    // Change state of background and logo
+    Future.delayed(const Duration(milliseconds: 800), () {
+      setState(() {
+        _visible = !_visible;
+      });
+    });
+
     // After 1000ms, go to program page
-    Timer(const Duration(milliseconds: 1000), () {
+    Future.delayed(const Duration(milliseconds: 1000), () {
       SharedPreferences.getInstance().then((prefs) {
         if (prefs.getBool('isFirstTime') == null) {
           prefs.setBool('isFirstTime', false);
-          Navigator.of(context).pushReplacementNamed(Routes.welcome);
+          Navigator.of(context).pushReplacementNamed(Routes.userDetails);
         } else {
           Navigator.of(context).pushReplacementNamed(Routes.program);
         }
       });
     });
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return _buildBody();
   }
 
   Widget _buildBody() {
-    return Stack(
-      children: <Widget>[
-        SizedBox.expand(
-          child: Image.asset(
-            'assets/background.jpg',
-            height: double.infinity,
-            alignment: Alignment.topCenter,
-            fit: BoxFit.fill,
+    return AnimatedOpacity(
+      opacity: _visible ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 300),
+      child: Stack(
+        children: <Widget>[
+          SizedBox.expand(
+            child: Image.asset(
+              'assets/background-nyp.jpg',
+              height: double.infinity,
+              alignment: Alignment.topCenter,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        SizedBox.expand(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: Image.asset('assets/logo.png'),
-              ),
-            ],
+          SizedBox.expand(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Image.asset('assets/logo.png'),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
